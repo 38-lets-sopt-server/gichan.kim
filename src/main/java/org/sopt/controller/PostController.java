c9f980bf2a32a20b9678478a4380464483f97ac2
gichanGim <k.kychan2003@gmail.com>
2026-04-10 00:42:47 +0900

    feat: 컨트롤러 로직 작성

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -1,13 +1,15 @@
 package org.sopt.controller;
 
+import org.sopt.common.ApiResponse;
 import org.sopt.dto.request.CreatePostRequest;
 import org.sopt.dto.response.CreatePostResponse;
 import org.sopt.dto.response.PostResponse;
+import org.sopt.exception.CustomException;
 import org.sopt.service.PostService;
 
 import java.util.List;
 
-class PostController {
+public class PostController {
     private final PostService postService = new PostService();
 
     // POST /posts 
@@ -20,24 +22,42 @@ class PostController {
     }
 
     // GET /posts 📝 과제
-    public List<PostResponse> getAllPosts() {
-        // TODO: postService.getAllPosts() 호출해서 반환
-        return null;
+    public ApiResponse<List<PostResponse>> getAllPosts() {
+        return ApiResponse.ok(postService.getAllPosts(), "게시글 목록 조회 성공.");
     }
 
     // GET /posts/{id} 📝 과제
-    public PostResponse getPost(Long id) {
+    public ApiResponse<PostResponse> getPost(Long id) {
         // TODO: postService.getPost(id) 호출, 예외 발생 시 null 반환
-        return null;
+        try{
+            return ApiResponse.ok(postService.getPost(id), "게시글 조회 성공.");
+        }
+        catch (CustomException e){
+            return ApiResponse.fail(e.getErrorCode().getStatus(), e.getErrorCode().getMessage());
+        }
     }
 
     // PUT /posts/{id} 📝 과제
-    public void updatePost(Long id, String newTitle, String newContent) {
+    public ApiResponse<Void> updatePost(Long id, String newTitle, String newContent) {
         // TODO: postService.updatePost() 호출, 예외 발생 시 에러 메시지 출력
+        try{
+            postService.updatePost(id, newTitle, newContent);
+            return ApiResponse.success(200, "게시글 수정 성공.");
+        }
+        catch (CustomException e){
+            return ApiResponse.fail(e.getErrorCode().getStatus(), e.getErrorCode().getMessage());
+        }
     }
 
     // DELETE /posts/{id} 📝 과제
-    public void deletePost(Long id) {
+    public ApiResponse<Void> deletePost(Long id) {
         // TODO: postService.deletePost() 호출, 예외 발생 시 에러 메시지 출력
+        try{
+            postService.deletePost(id);
+            return ApiResponse.success(200, "게시글 삭제 성공.");
+        }
+        catch (CustomException e){
+            return ApiResponse.fail(e.getErrorCode().getStatus(), e.getErrorCode().getMessage());
+        }
     }
 }
