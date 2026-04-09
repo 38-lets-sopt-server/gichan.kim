93fb9a32e859e7c94da95ae92a1fc7e7bed83b62
gichanGim <k.kychan2003@gmail.com>
2026-04-10 00:43:38 +0900

    feat: 서비스 로직 작성

--- src/main/java/org/sopt/service/PostService.java
+++ src/main/java/org/sopt/service/PostService.java
@@ -4,6 +4,8 @@ import org.sopt.domain.Post;
 import org.sopt.dto.request.CreatePostRequest;
 import org.sopt.dto.response.CreatePostResponse;
 import org.sopt.dto.response.PostResponse;
+import org.sopt.exception.CustomException;
+import org.sopt.exception.ErrorCode;
 import org.sopt.repository.PostRepository;
 
 import java.util.List;
@@ -27,23 +29,37 @@ public class PostService {
 
     // READ - 전체 📝 과제
     public List<PostResponse> getAllPosts() {
-        // TODO
-        return null;
+        return postRepository.findAll()
+                .stream()
+                .map(PostResponse::from)
+                .toList();
     }
 
     // READ - 단건 📝 과제
     public PostResponse getPost(Long id) {
-        // TODO
-        return null;
+        Post post = postRepository.findById(id);
+
+        if (post == null)
+            throw new CustomException(ErrorCode.POST_NOT_FOUND);
+
+        return PostResponse.from(post);
     }
 
     // UPDATE 📝 과제
     public void updatePost(Long id, String newTitle, String newContent) {
-        // TODO
+        Post post =  postRepository.findById(id);
+
+        if (post == null)
+            throw new CustomException(ErrorCode.POST_NOT_FOUND);
+
+        post.update(newTitle, newContent);
+        postRepository.deleteById(id);  // 기존 post는 삭제 처리
+        postRepository.save(post);
     }
 
     // DELETE 📝 과제
     public void deletePost(Long id) {
-        // TODO
+        if (!postRepository.deleteById(id))
+            throw new CustomException(ErrorCode.POST_NOT_FOUND);
     }
 }
