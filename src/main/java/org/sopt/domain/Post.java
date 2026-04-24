561d5c054a17ba7a26a52aadeb1c065ead187b86
gichanGim <k.kychan2003@gmail.com>
2026-04-24 15:12:54 +0900

    refactor: Post 엔티티 리펙토링

--- src/main/java/org/sopt/domain/Post.java
+++ src/main/java/org/sopt/domain/Post.java
@@ -1,32 +1,47 @@
 package org.sopt.domain;
 
+import org.sopt.dto.request.CreatePostRequest;
+import org.sopt.enums.BoardType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Post {
     private Long id;          // 게시글 상세 화면 — 특정 게시글 식별용
     private String title;     // 목록, 상세, 글쓰기 화면 — 제목
     private String content;   // 목록(미리보기), 상세(전체) 화면 — 내용
     private String author;    // 목록, 상세 화면 — 글쓴이
-    private String createdAt; // 목록, 상세 화면 — 작성 시각
+    BoardType boardType;    // 게시글 타입
+    private LocalDateTime createdAt; // 목록, 상세 화면 — 작성 시각
 
-    public Post(Long id, String title, String content, String author, String createdAt) {
+    private Post(Long id, String title, String content, String author, BoardType boardType, LocalDateTime createdAt) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.author = author;
+        this.boardType = boardType;
         this.createdAt = createdAt;
     }
 
-    public Long getId() { return id; }
-    public String getTitle() { return title; }
-    public String getContent() { return content; }
-    public String getAuthor() { return author; }
-    public String getCreatedAt() { return createdAt; }
+    public static Post createPost(Long id, CreatePostRequest request) {
+        return new Post(id, request.title(), request.content(), request.author(), request.boardType(), LocalDateTime.now());
+    }
 
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
     }
 
-    public String getInfo() {
-        return "[" + id + "] " + title + " - " + author + " (" + createdAt + ")\n" + content;
+    public void updateToHot(){
+        this.boardType = BoardType.HOT;
+    }
+
+    public Long getId() { return id; }
+    public String getTitle() { return title; }
+    public String getContent() { return content; }
+    public String getAuthor() { return author; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public BoardType getBoardType() {
+        return boardType;
     }
 }
