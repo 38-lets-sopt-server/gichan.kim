57e96cbeeea280e2c366b20a0aeb7ddbeb24f4fb
gichanGim <k.kychan2003@gmail.com>
2026-04-24 15:14:26 +0900

    feat: INVALID_BOARD_TYPE_VALUE 추가

--- src/main/java/org/sopt/exception/ErrorCode.java
+++ src/main/java/org/sopt/exception/ErrorCode.java
@@ -1,18 +1,19 @@
 package org.sopt.exception;
 
+import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다.");
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
+    INVALID_BOARD_TYPE_VALUE(HttpStatus.BAD_REQUEST, "잘못된 게시판 타입 값입니다.");
 
-    private final int status;
+    private final HttpStatus status;
     private final String message;
 
-
-    ErrorCode(int status, String message) {
+    ErrorCode(HttpStatus status, String message) {
         this.status = status;
         this.message = message;
     }
 
-    public int getStatus() { return status; }
+    public HttpStatus getStatus() { return status; }
     public String getMessage() { return message; }
 }
