f27c98dde73e87fa0bd02931c7b443f415383e2c
gichanGim <k.kychan2003@gmail.com>
2026-04-28 09:11:57 +0900

    feat: 레포지토리에 Jpa 적용

--- src/main/java/org/sopt/repository/PostRepository.java
+++ src/main/java/org/sopt/repository/PostRepository.java
@@ -1,14 +1,9 @@
 package org.sopt.repository;
 
 import org.sopt.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface PostRepository {
-    public Post save(Post post);
-    public List<Post> findAll();
-    public Optional<Post> findById(Long id);
-    public boolean deleteById(Long id);
-    public Long generateId();
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
