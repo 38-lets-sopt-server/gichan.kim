203ae99901020dc78af757bb58b1ad9714f3d95a
gichanGim <k.kychan2003@gmail.com>
2026-05-01 23:33:00 +0900

    feat: DeletePostRequest 추가

---
+++ src/main/java/org/sopt/post/dto/request/DeletePostRequest.java
@@ -0,0 +1,9 @@
+package org.sopt.post.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+// Spring Security 적용 이후 삭제 예정
+public record DeletePostRequest(
+        @NotNull
+        Long userId
+) {}
