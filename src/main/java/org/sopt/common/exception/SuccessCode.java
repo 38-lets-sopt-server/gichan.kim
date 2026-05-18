47ba1fe4ea8c97c74218360d6633adf919d20b9c
gichanGim <k.kychan2003@gmail.com>
2026-05-18 21:44:10 +0900

    feat: 성공 코드 추가

--- src/main/java/org/sopt/common/exception/SuccessCode.java
+++ src/main/java/org/sopt/common/exception/SuccessCode.java
@@ -9,7 +9,9 @@ public enum SuccessCode implements ResponseCode {
     POST_UPDATED(HttpStatus.OK, "게시글 수정 성공."),
     POST_DELETED(HttpStatus.OK, "게시글 삭제 성공."),
     LIKE_CREATED(HttpStatus.CREATED, "좋아요 추가 성공."),
-    LIKE_DELETED(HttpStatus.OK, "좋아요 취소 성공."),;
+    LIKE_DELETED(HttpStatus.OK, "좋아요 취소 성공."),
+    TOKEN_ISSUE_SUCCESS(HttpStatus.OK, "토큰 발급에 성공했습니다."),
+    GET_MY_INFO_SUCCESS(HttpStatus.OK, "내 정보 조회에 성공했습니다.");
 
     private final HttpStatus status;
     private final String message;
