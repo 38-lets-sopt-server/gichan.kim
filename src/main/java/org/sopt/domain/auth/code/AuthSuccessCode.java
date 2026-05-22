582546b8c0bfe943fa679c9df3446021f6aa4ed1
gichanGim <k.kychan2003@gmail.com>
2026-05-22 22:52:44 +0900

    feat: 인증 성공 코드 작성

--- src/main/java/org/sopt/domain/auth/code/AuthSuccessCode.java
+++ src/main/java/org/sopt/domain/auth/code/AuthSuccessCode.java
@@ -8,8 +8,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum AuthSuccessCode implements BaseCode {
-    TOKEN_ISSUE_SUCCESS(HttpStatus.OK, "토큰 발급에 성공했습니다."),
-    GET_MY_INFO_SUCCESS(HttpStatus.OK, "내 정보 조회에 성공했습니다.");
+    REFRESH_TOKEN_REISSUE_SUCCESS(HttpStatus.OK, "토큰 리프레시에 성공했습니다."),
+    GET_MY_INFO_SUCCESS(HttpStatus.OK, "내 정보 조회에 성공했습니다."),
+    SIGN_UP_SUCCESS(HttpStatus.CREATED, "회원가입에 성공했습니다."),
+    LOCAL_LOGIN_SUCCESS(HttpStatus.OK, "이메일 로그인에 성공했습니다."),
+    KAKAO_LOGIN_SUCCESS(HttpStatus.OK, "카카오 로그인에 성공했습니다."),
+    LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃에 성공했습니다.");
 
     private final HttpStatus status;
     private final String message;
