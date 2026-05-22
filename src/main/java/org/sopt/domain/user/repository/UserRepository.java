14723c3745141c7038062041f254445e176871af
gichanGim <k.kychan2003@gmail.com>
2026-05-22 22:56:13 +0900

    feat: 추가 메서드 작성

--- src/main/java/org/sopt/domain/user/repository/UserRepository.java
+++ src/main/java/org/sopt/domain/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package org.sopt.domain.user.repository;
 
+import org.sopt.domain.user.entity.AuthProvider;
 import org.sopt.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);  // 메서드 이름으로 쿼리 자동 생성
+    Optional<User> findByProviderAndProviderId(AuthProvider provider, String providerId);
+    boolean existsByEmail(String email);
 }
